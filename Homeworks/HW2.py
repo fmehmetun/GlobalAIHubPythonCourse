c2f7c3afec4a821698ee8854cb7e4fdd6b7d8abc
Furkan <fmehmetun3@gmail.com>
2021-04-07 21:57:45 +0300

    HW2 done

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,30 @@
-#Explain your work
+# 1 - Login system
 
-#Question 1
-for x in range(a):
-	print(a)
+# User records in dict
+users_dict = {
+	"fmehmetun": "verysimplepassword",
+	"user42": "notsosimplepassword",
+	"anon": "y0uc4nN0tpWnTh!s"
+}
+
+print("[*] Type 'exit' for exiting..")
+
+while True:
+	# Get username
+	username = input("[*] Username: ")
+
+	if username == "exit":
+		print("[*] Exiting..")
+		break
+
+	# Check if user exists
+	if username in users_dict:
+		# Get&check password for user
+		password = input("[*] Password: ")
+		if password == users_dict[username]:
+			print("[+] Login successful, welcome %s." % (username))
+			break
+		else:
+			print("[x] Invalid password for user %s.\n" % (username))
+	else:
+		print("[x] User %s doesn't exist.\n" % (username))
